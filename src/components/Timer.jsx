dd70adca3a1dfc2e2b2042861dee5890f9b16f19
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-06-03 16:39:22 -0700

    changed timer.jsx

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 import {useState,useEffect} from "react"
 export default function Timer({endTime}) {
 
-    const calculateTimeLeft=()=>{
-      const difference= new Date (endTime)-new Date();
-      if (difference<=0) 
-        return "Expired";
-      const hours= Math.floor(difference/(1000*60*60));
-      const minutes= Math.floor((difference % (1000*60*60))/(1000*60));
-      const seconds= Math.floor((difference %(1000*60))/1000);
-return `${hours}h ${minutes}m ${seconds}s`;
-    };
-    const [timeLeft ,setTimeLeft]= useState(calculateTimeLeft());
-    useEffect(()=>{
-      const interval=setInterval(()=>{
-        setTimeLeft(calculateTimeLeft());
-      },1000);
-      return ()=>clearInterval(interval);
-    }, [endTime]);
+    const [timeLeft, setTimeLeft] = useState("");
+
+useEffect(() => {
+  const calculateTimeLeft = () => {
+    const difference = new Date(endTime) - new Date();
+
+    if (difference <= 0) return "Expired";
+
+    const hours = Math.floor(difference / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return `${hours}h ${minutes}m ${seconds}s`;
+  };
+
+  setTimeLeft(calculateTimeLeft());
+
+  const interval = setInterval(() => {
+    setTimeLeft(calculateTimeLeft());
+  }, 1000);
+
+  return () => clearInterval(interval);
+}, [endTime]);
+
     return <span className="countdown-timer">{timeLeft}</span>;
   
     
